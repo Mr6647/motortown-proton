06bfac1667001a7bad2da8c3ed84d70d56ee8e23
Mr6647 <jonmohr@live.com>
2026-04-07 20:08:47 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -128,8 +128,8 @@ cd "${STEAM_APP_DIR}"
 
 exec "${PROTON_EXECUTABLE_PATH}" waitforexitandrun \
     "${STEAM_APP_DIR}/MotorTown/Binaries/Win64/MotorTownServer-Win64-Shipping.exe" \
-    Jeju_World?listen -QueryPort=27015 -server -log -stdout -FullStdOut -NoExit -useperfthreads \
-    -Port=7777 -bIsLanMatch=false
+    Jeju_World?listen?Port=7777?QueryPort=27015 -server -log -stdout -FullStdOut -NoExit -useperfthreads \
+    -bIsLanMatch=false
     
 # Post-hook
 source "${STEAM_HOME}/post.sh"
